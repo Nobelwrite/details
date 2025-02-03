8c0dd5264223cdee9061eb6a9919f44dea12f218
Nobelwrite <nobelwrite@gmail.com>
2025-02-03 20:15:05 +0100

    add requirement

--- details/details/settings.py
+++ details/details/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-809+ee2+*71alw+du01jca&ng&uhu%u5q$z*9n-eg1!)s4etat
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
